2393620807cfc0aeabf19b066f8c90611a24175d
André Ferreira <contato@andreferreira.me>
2018-04-21 11:06:33 -0300

    Title, Description, Favicon and Footer Links

--- scripts/main.js
+++ scripts/main.js
@@ -3,15 +3,18 @@ function calcReadingTime () {
   const words = document.getElementById('textarea').value.replace(/ +/g, ' ').trim().split(' ')
 
   if (words[0] !== '') {
-    const calc = words.length / 200
-    const minutes = Math.trunc(calc)
-    const seconds = Math.trunc((calc % 1).toFixed(4) * 0.6 * 100)
+    const calcMinutes = words.length / 200
+    const minutes = Math.trunc(calcMinutes)
+    const seconds = Math.trunc((calcMinutes % 1).toFixed(4) * 60)
 
-    document.getElementById('result').innerHTML = '<b>Estimated reading time:</b> ' + minutes + ' minutes and ' + seconds + ' seconds. Contains ' + words.length + ' words'
+    document.getElementById('result').innerHTML =
+      '<b>Estimated reading time:</b><br />' +
+      minutes + ' minutes and ' + seconds + ' seconds<br />' +
+      'Contains ' + words.length + ' words'
   }
 }
 
 function resetForm () {
   document.getElementById('textarea').value = ''
-  document.getElementById('result').innerHTML = 'Please <b>enter/paste</b> your text to estimate the reading time.'
+  document.getElementById('result').innerHTML = 'Please <b>enter/paste</b> your text'
 }
